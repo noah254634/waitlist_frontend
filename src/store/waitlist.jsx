486d5ebdb731c93d9cc2e5f1ed6d50e13eb66a68
IamKhaemba <noahkhaemba290@gmail.com>
2026-03-16 12:58:34 +0300

    added the V4ITE_API_URL

--- src/store/waitlist.jsx
+++ src/store/waitlist.jsx
@@ -1,22 +1,23 @@
 import { create } from "zustand";
-const render_url='https://waitlist-backend-m5ia.onrender.com/'
-const port = "http://localhost:3000"||`${render_url}}`;
+const API_URL = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : import.meta.env.VITE_API_URL;
 export const useStore = create((set) => ({
-    systemStats:async()=>{
-        try{
-            const response=await fetch(`${port}/api/v1/system_stats`,{
-                method:'GET',
-                headers:{
-                    'Content-Type':'application/json'
-                }
-            })
-            const data=await response.json()
-            console.log(data)
-            return data
-        }catch(err){
-            console.log(err)
-        }
-    },
+  systemStats: async () => {
+    try {
+      const response = await fetch(`${API_URL}/api/v1/system_stats`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  },
   postWaitlist: async (email, role) => {
     try {
       const response = await fetch(`${port}/api/v1/email_endpoint`, {
